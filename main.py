2a69b09dcf901d5b1a1f8178c1f92f0b70a5f193
Keira Tran <keira.e.tran@gmail.com>
2021-02-23 22:45:28 +0000

    Updated code in main, added image file, attempted to fix run error,

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
 import random
-
- # print('Aria: ' + reply)
-
-
 #Creating GUI with tkinter
 import tkinter
 from tkinter import *
+import re
 
 
+
+def start(): 
+  EntryBox.delete("0.0",END)
+  ChatLog.config(state=NORMAL)
+  ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+  reply = reply = "Hello! Welcome to ((insert store name)). We hope everything was to your liking. Ready to check out?"
+  ChatLog.insert(END, "Aria: " + str(reply) + '\n\n')
+  ChatLog.config(state=DISABLED)
+  ChatLog.yview(END)
+
+  #Create Button to checkout
+  checkoutButton = Button(base, font=("Verdana",12,'bold'), text="Checkout", width="12", height=5, bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff', command= checkout )
+  checkoutButton.place(x=6, y=401, height=90)
+
+
+#BOT'S RESPONSE BANK
 jokes = ["What do you call a pig that does karate? Pork chop.",
          "How do you organize a space party? You planet.",
          "What job did the frog have at the hotel? Bellhop.",
          "What do you call an alligator in a vest? An investigator.",
          "What do you call a lazy kangaroo? A pouch potato."]
 
-greetings = ["Hello.", "Hi!", "Hello there.", "Salutations.", "Greetings.", "Nice to meet you!", "Hiya!", "Nice to see you.", "Howdy."]
+greetings = ["Hello.", "Hi!", "Hello there.", "Salutations.", "Greetings.", "Nice to meet you!", "Nice to see you.", "Howdy."]
 greetings_lower = [i.lower() for i in ["Hello", "Hi!", "Hi", "Hello there", "Salutations", "Greetings", "Nice to meet you!", "Hiya!", "Nice to see you", "Howdy"]]
 
 gratitude_res = ["You're welcome!", "Happy to help!", "No problem!", "My pleasure!", "Have a great day!"]
 
-menu = ("\n__________________________\nDrinks\n__________________________\nLemonade: $2\nSoda: $2\nTea: $3\n\n__________________________\nBurgers\n__________________________\nCheeseburger: $5\nVeggie Burger: $12\nBacon Burger: $15\nChili Burger: $15\n\n__________________________\nSides\n__________________________\nFrench Fries: $5\nOnion Rings: $6\nPickles: $1\nMacaroni and Cheese: $5\n")
-
-lemonade = 2
-soda = 2
-tea = 3
-cheeseburger = 5
-veggie_burger = 12
-bacon_burger = 15
-chili_burger = 15
-french_fries = 5
-onion_rings = 6
-pickles = 1
-mac = 5
-
-order = []
-total = 0
-
+#TAKES TYPED MESSAGES FROM USER AND OUTPUTS A REPLY FROM BOT
 def getReply(message):
   if message != '': 
-
       if 'who' in message and 'you' in message:
           reply = "Hello, I'm Aria."
       elif 'joke' in message:
@@ -49,7 +45,7 @@ def getReply(message):
           reply = "Bye-bye! Thanks for chatting!"
       elif 'how are you' in message:
           reply = "I'm doing great! Thanks for asking!"
-      elif 'thank you' in message: 
+      elif 'thank' in message: 
           reply = random.choice(gratitude_res)
       elif 'menu' in message:
           reply = menu
@@ -57,19 +53,79 @@ def getReply(message):
           reply = "We're open every day from 10AM to 10PM."
       else:
           reply = "Sorry, I don't understand"
-      return reply
-      '''
-      elif 'order' in message:
-          place = input("Okay! Please write your order in this format\n\nCheeseburger, Cheeseburger, Veggie Burger, Lemonade\n\nRemember to separate your items by commas.")
-          for item in place:
-            list = str.split (",")
-            for i in list:
-                i.lower()
-                order.append(i)
-                total+= order(i)
-            reply = total
-      '''
+  return reply
 
+
+#ASKS THE USER TO SELECT ITEMS
+def checkout():
+  EntryBox.delete("0.0",END)
+  ChatLog.config(state=NORMAL)
+  ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+  reply = 'Great! Please enter the products that you would like to check out.'
+  ChatLog.insert(END, "Aria: " + str(reply) + '\n\n')
+  ChatLog.config(state=DISABLED)
+  ChatLog.yview(END)
+  listbox.place(x=60, y=131, height=150, width=265)
+
+  confirmButton = Button(base, font=("Verdana",12,'bold'), text="Confirm Items", width="12", height=5, bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff', command= confirm)
+  confirmButton.place(x=6, y=401, height=90)
+  
+  
+#THE USER CONFIRMS THEIR ORDER AND THE BOT RETURNS THE TOTAL 
+def confirm():  
+  EntryBox.delete("0.0",END)
+  ChatLog.config(state=NORMAL)
+  ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+
+  # Prints the order that the user made
+  values = [listbox.get(idx) for idx in listbox.curselection()]
+  print (values)
+
+  value = (', '.join(values))
+  print (value)
+  
+  #listbox.place(x=60, y=131, height=150, width=265)
+  listbox.destroy()
+  
+  # Prints the list of values
+  costPattern = r"\d+"
+  costMatch = re.findall(costPattern, value)
+  print (costMatch)
+
+  newTotal = [int(i) for i in costMatch] 
+  
+  # Printing modified list  
+  print ("Modified list is : " + str(newTotal)) 
+  
+  # Adding the values
+  total = 0
+  for ele in range(0, len(newTotal)):
+    total = total + newTotal[ele]
+ 
+  # Printing total value
+  print("Sum of all elements in given list: ", total)
+  
+  
+  replyConfirm = (', '.join(values))
+  replyTotal = 'Your total is: $' + str(total) + '. Thank you for shopping with us today!'
+  ChatLog.insert(END, "Aria: " + str(replyConfirm) + '\n\n')
+  ChatLog.insert(END, "Aria: " + str(replyTotal) + '\n\n')
+  ChatLog.config(state=DISABLED)
+  ChatLog.yview(END)
+  endButton = Button(base, font=("Verdana",12,'bold'), text="End Conversation", width="12", height=5, bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff', command= endConversation)
+  endButton.place(x=6, y=401, height=90)
+
+#ENDS THE CODE
+def endConversation():
+    base.destroy()
+
+#Unused code to return to the select purchase screen
+''''
+  backToCheckoutButton = Button(base, font=("Verdana",12,'bold'), text="Back To Checkout", width="15", height=5, bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff', command= checkout )
+  backToCheckoutButton.place(x=180, y=401, height=90)
+'''
+
+#PRESS TO SEND A MESSAGE TO BOT
 def send():
     message = EntryBox.get("1.0",'end-1c').strip()
     EntryBox.delete("0.0",END)
@@ -83,50 +139,86 @@ def send():
     ChatLog.config(state=DISABLED)
     ChatLog.yview(END)
 
+
+#SETTING UP WINDOWS
 base = Tk()
-base.title("Aria Bot")
+base.title("ChatBot")
 base.geometry("800x500")
 base.resizable(width=FALSE, height=FALSE)
 
+left_frame = Frame(base, width=370, height=370, bg='white')
+left_frame.grid(row=0, column=0, padx=10, pady=5)
+
+#ADDING TKINTER COMPONENTS
 #Create Chat window
 ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
-
 ChatLog.config(state=DISABLED)
 
 #Bind scrollbar to Chat window
 scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="arrow")
 ChatLog['yscrollcommand'] = scrollbar.set
 
-#Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5, bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff', command= send )
-
 #Create the box to enter message
 EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
 #EntryBox.bind("<Return>", send)
+
+#Create Button to start conversation
+startButton = Button(base, font=("Verdana",12,'bold'), text="Talk to Aria", width="12", height=5, bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff', command= start)
+
+#Create Button to send message
+SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="6", height=5, bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff', command= send )
+
+
+
+#Play gifs
+#Create frame that gif will play in
+right_frame = Frame(base, width=300, height=390, bg='white')
+right_frame.grid(row=0, column=1, padx=10, pady=5)
+
+# the label for intro_message 
+#intro_message = Label(base, text = "Press 'Send' to get started").place(x = 460, y = 30)   
+
+newCanvas = Canvas(right_frame, width=300, height=385, bg='white')
+newCanvas.grid(row=0, column=0, padx=0, pady=0)
+
+# load the .gif image file
+# put in your own gif file here, may need to add full path
+# like 'C:/WINDOWS/Help/Tours/WindowsMediaPlayer/Img/mplogo.gif'
+gif1 = PhotoImage(file = './images/test.gif')
+
+# put gif image on canvas
+#Later, this should include if statements to display different emotions
+newCanvas.create_image(150, 200, image = gif1)
+
+
+#CREATE LISTBOX
+listbox = Listbox(base, selectmode = "multiple", height = 10,  width = 15,  bg = "grey",  activestyle = 'dotbox',  font = "Helvetica", fg = "yellow") 
+    
+# insert elements by their 
+# index and names. 
+listbox.insert(1, "Soup: $13") 
+listbox.insert(2, "Pizza: $15") 
+listbox.insert(3, "Sorbet: $6") 
+listbox.insert(4, "Baguette: $4") 
+listbox.insert(5, "Burrito: $10") 
 
 
 #Place all components on the screen
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+startButton.place(x=6, y=401, height=90)
+EntryBox.place(x=440, y=401, height=90, width=265)
+SendButton.place(x=350, y=401, height=90)
 
-print ("Hello! My name is Aria, and I'm a restaurant bot! If you wish to end the chat, please type 'bye'\n")
-print ("Here's some special keywords that will help improve your dining experience.\n")
-print ("Type 'menu' to bring up our menu.\nType 'hours' to know our hours of operation.\n")
 
+#Run the code
 base.mainloop()
 
 
 
 def init_chat():
-
- # message = input("Hello! My name is Aria! Chat with me! If you wish to end the chat, please type 'bye'\n")
-
-  while message != "bye":
-    #Ask the user for more input, then use that in your response
-    message = input(getReply(message) + "\n")
-
+  #Ask the user for more input, then use that in your response
+  message = input(getReply(message) + "\n")
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 from main import reply
 import unittest
+import time
 
 class TestChatBot (unittest.TestCase):
   def raises_exception(self): 
+    start_time = time.time()
     with self.assertRaises(Exception):
       reply ("A")
+    end_time = time.time()
 
+    elapsed_time = end_time - start_time
+
+    print ("test_perf took" + str(elapsed_time) + "seconds.")
+
+    self.assertTrue (elapsed_time < 10)
 
 if __name__ == '__main__': 
   unittest.main()
